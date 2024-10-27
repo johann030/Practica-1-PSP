e0915b698aa781e5afa565b36c15835b9b8197af
Usuario <Usuario@DESKTOP-FNJ8BDH>
2024-10-27 22:11:24 +0100

    Comienzo

---
+++ Practica PSP Johann/src/holis/Holiwis.java
@@ -0,0 +1,13 @@
+package holis;
+
+public class Holiwis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "HOLA";
+
+		for (int i = 0; i < 15; i++) {
+			System.out.println(nombre + " " + i);
+		}
+	}
+}
